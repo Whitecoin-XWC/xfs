feb719df6cb151b016c95d8dc430debf677adb83
yuye1128 <yuyemail@163.com>
2021-06-01 15:06:35 +0800

    首次提交

--- src/main/java/com/nft/dao/entity/FilePO.java
+++ src/main/java/com/nft/dao/entity/FilePO.java
@@ -4,66 +4,84 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
+@ApiModel
 @TableName("nft_file")
 public class FilePO {
     /**
      * 主键
      */
+    @ApiModelProperty("tokenId")
     @TableId(value = "id", type = IdType.INPUT)
     private String id;
     /**
      * 标题
      */
+    @ApiModelProperty("标题")
     @TableField("file_title")
     private String fileTitle;
     /**
      * 文件名
      */
+    @ApiModelProperty("文件名")
     @TableField("file_name")
     private String fileName;
     /**
      * 文件路径
      */
+    @ApiModelProperty("文件存储路径")
     @TableField("file_path")
     private String filePath;
     /**
      * 付费时间，null 表示没付费
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty("付费时间")
     @TableField("pay_time")
     private Date payTime;
     /**
      * 创建时间
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty("创建时间")
     @TableField("create_time")
     private Date createTime;
     /**
      * 发布时间
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty("发布时间")
     @TableField("pub_time")
     private Date pubTime;
     /**
      * 用户标识
      */
+    @ApiModelProperty("用户标识")
     @TableField("user_tag")
     private String userTag;
     /**
      * 媒体类型 1 图片，2视频，3音频
      */
+    @ApiModelProperty("媒体类型，暂时没用")
     @TableField("media_type")
     private Integer mediaType;
     /**
      * 状态，1. 已上传，2. 已发布，3. 已付费
      */
+    @ApiModelProperty("状态 1. 已上传，2. 已发布，3. 已付费")
     @TableField("file_status")
     private Integer fileStatus;
     /**
      * 描述
      */
+    @ApiModelProperty("描述")
     @TableField("file_des")
     private String fileDes;
 }
--- src/main/java/com/nft/service/dto/FileDTO.java
+++ src/main/java/com/nft/service/dto/FileDTO.java
@@ -2,16 +2,22 @@ package com.nft.service.dto;
 
 import com.nft.dao.entity.FileLogPO;
 import com.nft.dao.entity.FilePO;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@ApiModel
 public class FileDTO {
 
+    @ApiModelProperty("文件数据")
     private FilePO filePO;
 
+    @ApiModelProperty("操作日志")
     private List<FileLogPO> fileLogPOList;
 
+    @ApiModelProperty("图片url前缀")
     private String imgUrl;
 }
