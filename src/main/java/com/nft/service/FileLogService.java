fa90f81f95427444aee9126f28a7f7d33450dbd4
yuye1128 <yuyemail@163.com>
2021-07-12 17:52:48 +0800

    首次提交

--- src/main/java/com/nft/service/FileLogService.java
+++ src/main/java/com/nft/service/FileLogService.java
@@ -6,9 +6,10 @@ public interface FileLogService {
 
     /**
      * 保存日志
-     * @param fileId
-     * @param action
-     * @param type
+     * @param fileId  文件ID，也就是文件的tokenId
+     * @param action 日志动作描述，比如发布了nft，出售了nft
+     * @param type 0 普通变化，1 竞拍，2 出售
+     * @param fileLogAttach 日志携带的附件，用于记录更多的数据
      * @return
      */
     int saveLog(String fileId, String action, Integer type, FileLogAttach fileLogAttach);
