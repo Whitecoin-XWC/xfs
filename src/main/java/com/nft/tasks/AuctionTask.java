eb907d2c9674d845ca18604d9d8c39ea0328c031
moloqu <moloqu@yeah.net>
2021-07-27 16:36:18 +0800

    modify for auction task

--- src/main/java/com/nft/tasks/AuctionTask.java
+++ src/main/java/com/nft/tasks/AuctionTask.java
@@ -2,8 +2,12 @@ package com.nft.tasks;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.nft.dao.entity.AuctionEntity;
+import com.nft.dao.entity.AuctionHistoryEntity;
 import com.nft.dao.mapper.AuctionMapper;
+import com.nft.service.AuctionHistoryService;
 import com.nft.service.AuctionService;
+import com.nft.service.FileLogService;
+import com.nft.service.dto.FileLogAttach;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -24,6 +28,9 @@ public class AuctionTask {
     @Resource
     private AuctionService auctionService;
 
+    @Resource
+    private FileLogService fileLogService;
+
     /**
      * 修改拍卖倒计时结束的记录
      */
@@ -34,11 +41,16 @@ public class AuctionTask {
         //TODO 修改拍卖时长
         LocalDateTime time = LocalDateTime.now().minusMinutes(10);
         queryWrapper.le("auction_start_time", time);
+        queryWrapper.eq("auction_status", 1);
         List<AuctionEntity> auctionEntities = auctionService.list(queryWrapper);
         for (AuctionEntity auctionEntity : auctionEntities) {
             auctionEntity.setAuctionStatus(AUCTION_END.getStatuCode());
             auctionEntity.setUpdateTime(new Date());
         }
         auctionService.updateBatchById(auctionEntities);
+        /* 插入拍卖结束的记录 */
+        for (AuctionEntity auctionEntity : auctionEntities) {
+            fileLogService.saveLog(auctionEntity.getFileTokenId(), "拍卖结束", "", 1, new FileLogAttach());
+        }
     }
 }
