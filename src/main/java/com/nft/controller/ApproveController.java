c53b6f83f4e4dfa59423ba5b1b1fb4f696d457de
moloqu <moloqu@yeah.net>
2021-09-03 16:41:52 +0800

    approve add log

--- src/main/java/com/nft/controller/ApproveController.java
+++ src/main/java/com/nft/controller/ApproveController.java
@@ -1,6 +1,8 @@
 package com.nft.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
@@ -11,11 +13,13 @@ import java.net.UnknownHostException;
  * Created by moloq on 2021/9/3 10:01
  */
 @RestController
+@Slf4j
 public class ApproveController {
 
-    @GetMapping("/approve")
-    public String approve() throws UnknownHostException {
+    @PostMapping("/approve")
+    public String approve(@RequestBody String message) throws UnknownHostException {
         InetAddress address = InetAddress.getLocalHost();
+        log.info(message);
         return address.getHostAddress();
     }
 }
